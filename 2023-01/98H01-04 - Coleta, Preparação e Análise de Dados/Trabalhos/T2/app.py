05b3174ee09c24a5ac494489d0510c110398d4e9
Cristiano Moreira Martins <cristiano.mrmartins@gmail.com>
2023-04-24 00:00:34 -0300

    finalizado API e jupyter NB

---
+++ 2023-01/98H01-04 - Coleta, Preparação e Análise de Dados/Trabalhos/T2/app.py
@@ -0,0 +1,78 @@
+'''
+Nome: Cristiano Moreira Martins
+Matrícula: 11111871-7
+'''
+
+
+from flask import Flask, request, jsonify, make_response
+import pandas_datareader as pdr
+
+app = Flask(__name__)
+
+def load_data():
+    global symbols_data
+    symbols_data = pdr.nasdaq_trader.get_nasdaq_symbols()
+    symbols_data = symbols_data.rename(
+        columns = {
+            'NASDAQ Symbol': 'symbol',
+            'Nasdaq Traded': 'isTraded',
+            'Security Name': 'name',
+            'Listing Exchange': 'exchange',
+            'Market Category': 'category',
+            'ETF': 'isETF',   
+            'Round Lot Size': 'lotSize',
+            'Test Issue': 'isTest',
+            'Financial Status': 'status',
+            'CQS Symbol': 'cqsSymbol',
+            'NextShares': 'nextShares'
+        })
+    symbols_data = symbols_data.to_dict(orient='records')
+    print(f'Dados de {len(symbols_data)} instrumentos carregados.')
+
+def find_symbol(symbol):
+    for index, item in enumerate(symbols_data):
+        if item['symbol'] == symbol:
+            return index
+    return None
+
+@app.route('/api/symbols', methods=['GET', 'POST'])
+def symbols():
+    if request.method == 'GET':
+        page = int(request.args.get("page", 1))
+        per_page = int(request.args.get("per_page", 10))
+        start = (page - 1) * per_page
+        end = start + per_page
+        paginated_symbols = symbols_data[start:end]
+        return jsonify(paginated_symbols)
+    elif request.method == 'POST':
+        new_symbol = request.json
+        symbol = new_symbol['symbol']
+        if find_symbol(symbol) is not None:
+            return make_response(jsonify({'error': 'O instrumento já existe.'}), 400)
+        else:
+            symbols_data.append(new_symbol)
+            return make_response(jsonify({'message': f'Instrumento adicionado.'}), 201)
+
+@app.route('/api/symbols/<symbol>', methods=['GET', 'PUT', 'DELETE'])
+def symbol(symbol):
+    symbol_index = find_symbol(symbol)
+
+    if symbol_index is None:
+        return make_response(jsonify({'error': 'Instrumento não encontrado.'}), 404)
+
+    if request.method == 'GET':
+        return jsonify(symbols_data[symbol_index])
+
+    elif request.method == 'PUT':
+        updated_symbol = request.json
+        symbols_data[symbol_index] = updated_symbol
+        return jsonify({'message': 'Instrumento atualizado.'})
+
+    elif request.method == 'DELETE':
+        del symbols_data[symbol_index]
+        return jsonify({'message': 'Instrumento removido.'})
+
+if __name__ == '__main__':
+    print('Iniciando a API...')
+    load_data()
+    app.run(debug=False)
